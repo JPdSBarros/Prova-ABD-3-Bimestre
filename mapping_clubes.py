d1c84ee32042b8685635d85980816d3b7b34da6c
João Paulo da Silva Barros <114430817+JPdSBarros@users.noreply.github.com>
2022-10-02 14:33:29 -0300

    Add files via upload

---
+++ mapping_clubes.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
+
+URL="mysql+mysqlconnector://localhost/intro_orm"
+engine = create_engine(url=URL)
+
+Base = declarative_base(orm_programa_clubes)
+
+class Pessoa(Base):
+    __tabela__ = "Pessoa"
+    CPF = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    natalidade = Column(Date, nullable=False)
+
+class Dono(Base):
+    __tabela__ = "Donos"
+    id_dono = Column(Integer, primary_key=True)
+    cpf = Column(Integer, nullable=False)
+    data_integracao = Column(Date, nullable=False)
+
+class Membro(Base):
+    __tabela__ = "Membros"
+    id_membro = Column(Integer, primary_key=True)
+    cpf = Column(Integer, nullable=False)
+    data_integracao = Column(Date, nullable=False)
+
+class Clube(Base):
+    __tabela__ = "Clubes"
+    id_Clube = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    id_dono = Column(Integer, nullable=False)
+    quantidade_membros = Column(Integer, nullable=False)
+    locais= Column(Integer, nullable=False)
+
+class Local(Base):
+    __tabela__ = "Locais"
+    CNPJ = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    endereço = Column(String(100), nullable=False) 
+    horarios = Column(Date,nullable=False)
+
+
+
+Base.metadata.create_all(engine)
